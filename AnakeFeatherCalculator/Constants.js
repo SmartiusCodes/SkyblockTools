6c333d1743e9f40db9fdec4de1ab18d1ab0623c9
Smartius <marharstrand1@gmail.com>
2025-06-20 05:17:02 +0200

    Anake Feather

---
+++ AnakeFeatherCalculator/Constants.js
@@ -0,0 +1,196 @@
+var constants = {
+
+    anake_feather: {
+
+        experimentation_table: {
+            xp: 100000
+        },
+
+        slayers: {
+            revenant: 500000,
+            tarantula: 0,
+            sven: 300000,
+            voidgloom: 70000,
+            riftstalker: 8000,
+            inferno: 85000
+        },
+
+        catacombs: {
+            f1: 10000,
+            f2: 10000,
+            f3: 10000,
+            f4: 10000,
+            f5: 40000,
+            f6: 30000,
+            f7: 8000,
+            m1: 7000,
+            m2: 7000,
+            m3: 12000,
+            m4: 12000,
+            m5: 20000,
+            m6: 20000,
+            m7: 7000
+        },
+
+        crystal_nucleus: {
+            xp: 17000
+        }
+
+    },
+
+    items: {
+
+        experimentation_table: {
+            bazaar_items: {
+                TITANIC_EXP_BOTTLE: 'Titanic Experience Bottle',
+                METAPHYSICAL_SERUM: 'Metaphysical Serum',
+                ENCHANTMENT_SCAVENGER_5: 'Scavenger 5',
+                GOLDEN_BOUNTY: 'Golden Bounty',
+                ENCHANTMENT_SHARPNESS_6: 'Sharpness 6',
+                ENCHANTMENT_SHARPNESS_7: 'Sharpness 7',
+                ENCHANTMENT_LIFE_STEAL_4: 'Life Steal 4',
+                ENCHANTMENT_LIFE_STEAL_5: 'Life Steal 5',
+                ENCHANTMENT_POWER_6: 'Power 6',
+                ENCHANTMENT_POWER_7: 'Power 7',
+                ENCHANTMENT_ENDER_SLAYER_6: 'Ender Slayer 6',
+                ENCHANTMENT_ENDER_SLAYER_7: 'Ender Slayer 7',
+                ENCHANTMENT_THUNDERBOLT_6: 'Thunderbolt 6',
+                ENCHANTMENT_THUNDERBOLT_7: 'Thunderbolt 7',
+                ENCHANTMENT_GROWTH_6: 'Growth 6',
+                ENCHANTMENT_GROWTH_7: 'Growth 7',
+                ENCHANTMENT_CHANCE_4: 'Chance 4',
+                ENCHANTMENT_CHANCE_5: 'Chance 5',
+                ENCHANTMENT_BLAST_PROTECTION_6: 'Blast Protection 6',
+                ENCHANTMENT_BLAST_PROTECTION_7: 'Blast Protection 7',
+                ENCHANTMENT_PROJECTILE_PROTECTION_6: 'Projectile Protection 6',
+                ENCHANTMENT_PROJECTILE_PROTECTION_7: 'Projectile Protection 7',
+                ENCHANTMENT_FIRE_PROTECTION_6: 'Fire Protection 6',
+                ENCHANTMENT_FIRE_PROTECTION_7: 'Fire Protection 7',
+                ENCHANTMENT_GIANT_KILLER_6: 'Giant Killer 6',
+                ENCHANTMENT_GIANT_KILLER_7: 'Giant Killer 7',
+                ENCHANTMENT_SYPHON_4: 'Syphon 4',
+                ENCHANTMENT_SYPHON_5: 'Syphon 5',
+                ENCHANTMENT_PROTECTION_6: 'Protection 6',
+                ENCHANTMENT_PROTECTION_7: 'Protection 7',
+                ENCHANTMENT_TITAN_KILLER_6: 'Titan Killer 6',
+                ENCHANTMENT_TITAN_KILLER_7: 'Titan Killer 7',
+                PESTHUNTING_GUIDE: "A Beginner's Guide To Pesthunting",
+                SEVERED_PINCER: 'Severed Pincer',
+                ENCHANTMENT_THUNDERLORD_6: 'Thunderlord 6',
+                ENCHANTMENT_CRITICAL_7: 'Critical 7',
+                ENCHANTMENT_BANE_OF_ARTHROPODS_7: 'Bane of Arthropods 7',
+                ENCHANTMENT_SNIPE_4: 'Snipe 4',
+                GOLD_BOTTLE_CAP: 'Gold Bottle Cap',
+                ENCHANTMENT_LOOTING_5: 'Looting 5',
+                ENCHANTMENT_FIRST_STRIKE_5: 'First Strike 5',
+                ENCHANTMENT_CUBISM_6: 'Cubism 6',
+                ENCHANTMENT_TRIPLE_STRIKE_5: 'Triple Strike 5',
+                ENCHANTMENT_SMITE_7: 'Smite 7',
+                CHAIN_END_TIMES: 'Chain Of The End Times',
+                ENCHANTMENT_CLEAVE_6: 'Cleave 6',
+                OCTOPUS_TENDRIL: 'Octopus Tendril',
+                ENCHANTMENT_LUCK_7: 'Luck 7',
+                ENCHANTMENT_EXECUTE_6: 'Execute 6',
+                TROUBLED_BUBBLE: 'Troubled Bubble',
+                ENCHANTMENT_VENOMOUS_6: 'Venomous 6',
+                ENCHANTMENT_PROSECUTE_6: 'Prosecute 6'
+            },
+            auction_items: {
+                1: 'Nadeshiko Dye'
+            }
+        },
+
+        slayers: {
+            revenant: {
+                bazaar_items: {
+                    FOUL_FLESH: 'Foul Flesh',
+                    UNDEAD_CATALYST: 'Undead Catalyst',
+                    ENCHANTMENT_SMITE_6: 'Smite 6',
+                    ENCHANTMENT_SMITE_7: 'Smite 7',
+                    REVENANT_CATALYST: 'Revenant Catalyst',
+                    REVENANT_VISCERA: 'Revenant Viscera'
+                },
+                auction_items: {
+                    1: 'Matcha Dye',
+                    2: 'Pestilence Rune 1',
+                    3: 'Beheaded Horror',
+                    4: 'Snake Rune 1',
+                    5: 'Scythe Blade',
+                    6: 'Shard of the Shredded',
+                    7: 'Warden Heart'
+                }
+            },
+            tarantula: {
+                bazaar_items: {
+                    TOXIC_ARROW_POISON: 'Toxic Arrow Poison',
+                    SPIDER_CATALYST: 'Spider Catalyst',
+                    ENCHANTMENT_BANE_OF_ARTHROPODS_6: 'Bane of Arthropods 6',
+                    FLY_SWATTER: 'Fly Swatter'
+                },
+                auction_items: {
+                    1: 'Brick Red Dye',
+                    2: 'Bite Rune 1',
+                    3: 'Tarantula Talisman',
+                    4: 'Digested Mosquito'
+                }
+            },
+            sven: {
+                bazaar_items: {
+                    HAMSTER_WHEEL: 'Hamster Wheel',
+                    ENCHANTMENT_CRITICAL_6: 'Critical 6',
+                    FURBALL: 'Furball'
+                },
+                auction_items: {
+                    1: 'Celeste Dye',
+                    2: 'Spirit Rune 1',
+                    3: 'Red Claw Egg',
+                    4: 'Courture Rune 1',
+                    5: 'Overflux Capacitor',
+                    6: 'Grizzly Salmon'
+                }
+            },
+            voidgloom: {
+                bazaar_items: {
+                    TWILIGHT_ARROW_POISON: 'Twilight Arrow Poison',
+                    SUMMONING_EYE: 'Summoning Eye',
+                    ENCHANTMENT_MANA_STEAL_1: 'Mana Steal 1',
+                    TRANSMISSION_TUNER: 'Transmission Tuner',
+                    NULL_ATOM: 'Null Atom',
+                    HAZMAT_ENDERMAN: 'Hazmat Enderman',
+                    ENCHANTMENT_SMARTY_PANTS_1: 'Smarty Pants 1',
+                    ENCHANTMENT_ENDER_SLAYER_7: 'Ender Slayer 7'
+                },
+                auction_items: {
+                    1: 'Byzantium Dye',
+                    2: 'Endersnake Rune 1',
+                    3: 'Pocket Espresso Machine',
+                    4: 'End Rune 1',
+                    5: 'Handy Blood Chalice',
+                    6: 'Sinful Dice',
+                    7: 'Exceedingly Rare Ender Artifact Upgrade',
+                    8: 'Void Conqueror Enderman Skin',
+                    9: 'Etherwarp Merger',
+                    10: 'Judgement Core',
+                    11: 'Enchant Rune 1'
+                }
+            },
+            riftstalker: {
+                bazaar_items: {
+                    BUBBA_BLISTER: 'Bubba Blister',
+                    CHOCOLATE_CHIP: 'Fang-tastic Chocolate Chip',
+                    GUARDIAN_LUCKY_BLOCK: 'Guardian Lucky Block',
+                    ENCHANTMENT_QUANTUM_3: 'Enchanted Book Bundle (Quantum)',
+                    ENCHANTMENT_ULTIMATE_THE_ONE_4: 'Enchanted Book Bundle (The One)'
+                },
+                auction_items: {
+                    1: 'Sangria Dye'
+                }
+            },
+            inferno: {
+                
+            }
+        }
+
+    }
+
+}
